ecf82f9444feefa22f97495e7a5bec7c39560a12
Emir Cem Gezer <emircem.gezer@gmail.com>
2017-08-16 18:54:17 +0300

    everything you need

--- scripts/robot.py
+++ scripts/robot.py
@@ -2,17 +2,13 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import rospy
-from geometry_msgs.msg import Twist, PoseStamped
+from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import LaserScan, Image
-from visualization_msgs.msg import Marker
+from sensor_msgs.msg import LaserScan
 
 import tf
-import cv2
-import cv_bridge
 import math
-import time
-from helpers import calculate_difference, find_minima, get_laser_angle
+from helpers import calculate_difference, find_minima
 
 
 class Robot(object):
@@ -21,7 +17,7 @@ class Robot(object):
         self.name = name
 
         # Publisher
-        self.cmd_vel = rospy.Publisher( self.name + "/cmd_vel",
+        self.cmd_vel = rospy.Publisher( self.name + "/cmd_vel_mux/input/teleop",
                                        Twist, queue_size=10)
 
         # Subscriber
@@ -29,31 +25,12 @@ class Robot(object):
                                      Odometry, self.odom_callback,
                                      queue_size=1)
 
-        self.laser = rospy.Subscriber( self.name + "/front_laser/scan",
+        self.laser = rospy.Subscriber( self.name + "/scan",
                                      LaserScan, self.laser_callback,
                                       queue_size=1)
 
-        self.camera = rospy.Subscriber( self.name + "/front_camera/image_raw",
-                                       Image, self.camera_callback,
-                                       queue_size=1)
-
-        self.marker = rospy.Subscriber( self.name + "/marker",
-                                       Marker, self.marker_callback,
-                                       queue_size=1)
-
-        self.nav_goal = rospy.Subscriber("/move_base_simple/goal",
-                                       PoseStamped, self.nav_goal_callback,
-                                         queue_size=1)
-
         self.pose_data = None
         self.laser_data = None
-        self.camera_data = None
-        self.marker_data = None
-        self.nav_goal = None
-        self.obs_distance = None
-        self.obs_angle = None
-        self.cv_bridge = cv_bridge.CvBridge()
-
         self.rate = rospy.Rate(10)
         self.rate.sleep()
 
@@ -84,30 +61,12 @@ class Robot(object):
 
         :param msg: LaserScan callback data
         """
-        if msg is None: return
+        if msg is None:
+            return
         self.laser_data = np.array(msg.ranges)
         minima = find_minima(self.laser_data)
-        if len(minima) is 0: return
-
-
-    def camera_callback(self, msg):
-        try:
-            self.camera_data = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
-        except cv_bridge.CvBridgeError:
+        if len(minima) is 0:
             return
-
-        gray = cv2.cvtColor(self.camera_data, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (5, 5), 0)
-        canny = cv2.Canny(blur, 30, 150)
-
-        # cv2.imshow("Robot Camera", canny)
-        cv2.waitKey(1)
-
-    def marker_callback(self, msg):
-        self.marker_data = msg
-
-    def nav_goal_callback(self, msg):
-        self.nav_goal = [msg.pose.position.x, msg.pose.position.y]
 
     def set_speed(self, linear, angular):
         movecmd = Twist()
